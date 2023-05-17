6554fe6557c41b1cc58eb13b6d79015f899255bc
Vedant <vedantnp200@gmail.com>
2023-05-18 00:04:24 +0530

    Addition code is added

--- Program.cs
+++ Program.cs
@@ -4,9 +4,23 @@ namespace Calculator
 {
     class Program
     {
+        static void addition()
+        {
+            int a, b, result;
+
+            Console.WriteLine("Enter first Number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            result = a + b;
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, result);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculator");   
+            Console.WriteLine("Calculator"); 
+            Program.addition();
         }
     }
 }
