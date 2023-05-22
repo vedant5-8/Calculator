1a20a6e72644c39f96be28f65785ffeaeb30d236
Vedant <vedantnp200@gmail.com>
2023-05-22 17:21:28 +0530

    Addition of two program is added

---
+++ Addition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Addition
+    {
+        public static void Add()
+        {
+            int a, b, result;
+
+            Console.WriteLine("Enter first Number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            result = a + b;
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, result);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,25 +4,44 @@ namespace Calculator
 {
     class Program
     {
-        static void Addition()
-        {
-            int a, b, result;
-
-            Console.WriteLine("Enter first Number: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second Number: ");
-            b = Convert.ToInt32(Console.ReadLine());
-
-            result = a + b;
-
-            Console.WriteLine("{0} + {1} = {2}", a, b, result);
-        }
-
+        
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculator"); 
-            Program.Addition();
+            while (true)
+            {
+                Console.WriteLine("Choose a method: ");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Substraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("=>");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Addition.Add();
+                        break;
+                    case 2:
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct choice.");
+                        break;
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
